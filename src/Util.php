b439723fa7648f8a1b7e49067f500daaf1c5d5b3
Fabio Dias <fabio@studiogt.com.br>
2018-04-18 11:59:09 -0300

    Voltando a aceitar acentos

--- src/Util.php
+++ src/Util.php
@@ -7,66 +7,6 @@ class Util {
 		return str_pad($input, strlen($input)-mb_strlen($input,$encoding)+$pad_length, $pad_string, $pad_style); 
 	}
 
-	public static function remover_acentos($str) {
-		$foreign_characters = array(
-			'/ä|æ|ǽ/' => 'ae',
-			'/ö|œ/' => 'oe',
-			'/ü/' => 'ue',
-			'/Ä/' => 'Ae',
-			'/Ü/' => 'Ue',
-			'/Ö/' => 'Oe',
-			'/À|Á|Â|Ã|Ä|Å|Ǻ|Ā|Ă|Ą|Ǎ/' => 'A',
-			'/à|á|â|ã|å|ǻ|ā|ă|ą|ǎ|ª/' => 'a',
-			'/Ç|Ć|Ĉ|Ċ|Č/' => 'C',
-			'/ç|ć|ĉ|ċ|č/' => 'c',
-			'/Ð|Ď|Đ/' => 'Dj',
-			'/ð|ď|đ/' => 'dj',
-			'/È|É|Ê|Ë|Ē|Ĕ|Ė|Ę|Ě/' => 'E',
-			'/è|é|ê|ë|ē|ĕ|ė|ę|ě/' => 'e',
-			'/Ĝ|Ğ|Ġ|Ģ/' => 'G',
-			'/ĝ|ğ|ġ|ģ/' => 'g',
-			'/Ĥ|Ħ/' => 'H',
-			'/ĥ|ħ/' => 'h',
-			'/Ì|Í|Î|Ï|Ĩ|Ī|Ĭ|Ǐ|Į|İ/' => 'I',
-			'/ì|í|î|ï|ĩ|ī|ĭ|ǐ|į|ı/' => 'i',
-			'/Ĵ/' => 'J',
-			'/ĵ/' => 'j',
-			'/Ķ/' => 'K',
-			'/ķ/' => 'k',
-			'/Ĺ|Ļ|Ľ|Ŀ|Ł/' => 'L',
-			'/ĺ|ļ|ľ|ŀ|ł/' => 'l',
-			'/Ñ|Ń|Ņ|Ň/' => 'N',
-			'/ñ|ń|ņ|ň|ŉ/' => 'n',
-			'/Ò|Ó|Ô|Õ|Ō|Ŏ|Ǒ|Ő|Ơ|Ø|Ǿ/' => 'O',
-			'/ò|ó|ô|õ|ō|ŏ|ǒ|ő|ơ|ø|ǿ|º/' => 'o',
-			'/Ŕ|Ŗ|Ř/' => 'R',
-			'/ŕ|ŗ|ř/' => 'r',
-			'/Ś|Ŝ|Ş|Š/' => 'S',
-			'/ś|ŝ|ş|š|ſ/' => 's',
-			'/Ţ|Ť|Ŧ/' => 'T',
-			'/ţ|ť|ŧ/' => 't',
-			'/Ù|Ú|Û|Ũ|Ū|Ŭ|Ů|Ű|Ų|Ư|Ǔ|Ǖ|Ǘ|Ǚ|Ǜ/' => 'U',
-			'/ù|ú|û|ũ|ū|ŭ|ů|ű|ų|ư|ǔ|ǖ|ǘ|ǚ|ǜ/' => 'u',
-			'/Ý|Ÿ|Ŷ/' => 'Y',
-			'/ý|ÿ|ŷ/' => 'y',
-			'/Ŵ/' => 'W',
-			'/ŵ/' => 'w',
-			'/Ź|Ż|Ž/' => 'Z',
-			'/ź|ż|ž/' => 'z',
-			'/Æ|Ǽ/' => 'AE',
-			'/ß/'=> 'ss',
-			'/Ĳ/' => 'IJ',
-			'/ĳ/' => 'ij',
-			'/Œ/' => 'OE',
-			'/ƒ/' => 'f'
-		);
-		$link = strtolower(preg_replace(array_keys($foreign_characters), array_values($foreign_characters), $str));
-		$link = preg_replace("/[^a-z0-9]/",' ',$link);
-		$link = preg_replace("/\s+/",' ',$link);
-		$link = trim($link);
-		return $link;
-	}
-
 	public static function format($formato, $valor) {
 		$pad_style = STR_PAD_LEFT;
 		$pad_string = '0';
@@ -75,7 +15,6 @@ class Util {
 			$pad_length = (int)$matches['tamanho'];
 			$pad_style = STR_PAD_RIGHT;
 			$pad_string = ' ';
-			$valor = static::remover_acentos($valor);
 		} else if (preg_match('/^9\((?P<inteiro>\d+)\)v[9]?\((?P<decimal>\d+)\)$/', $formato, $matches)) {
 			$valor = preg_replace('/[^\d\.\-\,]/','',$valor);
 			$inteiro = (int)$matches['inteiro'];
